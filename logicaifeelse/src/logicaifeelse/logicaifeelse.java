36fb19d68b773e9b4ebd10e501fe6763a707a90e
LEOCRDEV leocrdev@hotmail.com <LEOCRDEV leocrdev@hotmail.com>
2022-10-16 10:36:32 -0300

    Aula de logica if e else

---
+++ logicaifeelse/src/logicaifeelse/logicaifeelse.java
@@ -0,0 +1,24 @@
+package logicaifeelse;
+
+public class logicaifeelse {
+
+	
+	/* Main é um método auto executável em Java */
+	public static void main(String[] args) {
+		
+		
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 70;
+		int nota4 = 80;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		if (media >= 70) {
+			System.out.println("Aluno Aprovado: " + media);
+		}else {
+			System.out.println("Aluno Reprovado: " + media);
+		}
+	}
+}
