d3b0bad784b870a555f0d38008f21756209bb4c9
LEOCRDEV leocrdev@hotmail.com <LEOCRDEV leocrdev@hotmail.com>
2022-10-16 10:47:44 -0300

    organizando código

--- logicaifeelse/src/logicaifeelse/logicaifeelse.java
+++ logicaifeelse/src/logicaifeelse/logicaifeelse.java
@@ -2,11 +2,9 @@ package logicaifeelse;
 
 public class logicaifeelse {
 
-	
 	/* Main é um método auto executável em Java */
 	public static void main(String[] args) {
-		
-		
+
 		int nota1 = 10;
 		int nota2 = 60;
 		int nota3 = 70;
@@ -14,14 +12,12 @@ public class logicaifeelse {
 		int media = 0;
 
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
-		
+
 		if (media >= 70) {
 			System.out.println("Aluno Aprovado: " + media);
-		}
-		if (media >= 40 && media <= 69) {
+		} else if (media >= 40 && media <= 69) {
 			System.out.println("Aluno está de recuperação: " + media);
-		}
-		else {
+		} else {
 			System.out.println("Aluno Reprovado: " + media);
 		}
 	}
